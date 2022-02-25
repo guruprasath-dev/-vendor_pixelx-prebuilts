1002afb4f2ab5098d06330c1bf8e82cc8f920a2a
guruprasath-dev <70852423+guruprasath-dev@users.noreply.github.com>
2022-02-25 09:36:16 +0530

    changed Android.mk

--- packages/apps/PixelXTuner/Android.mk
+++ packages/apps/PixelXTuner/Android.mk
@@ -1,17 +1,21 @@
 LOCAL_PATH := $(call my-dir)
 
+my_archs := arm x86 arm64
+my_src_arch := $(call get-prebuilt-src-arch, $(my_archs))
+
 include $(CLEAR_VARS)
-
-LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := PixelXTuner
-LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_BUILT_MODULE_STEM := package.apk
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := PixelXTuner.apk
 
-LOCAL_PRIVILEGED_MODULE = true
-# LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)
-# LOCAL_OVERRIDES_PACKAGES := Home Launcher2  Launcher3
+LOCAL_PREBUILT_JNI_LIBS := \
+  @lib/arm64-v8a/libflutter.so \
+  @lib/arm64-v8a/libapp.so
 
-LOCAL_MODULE_SUFFIX :=  $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_TARGET_ARCH := $(my_src_arch)
 
 include $(BUILD_PREBUILT)
